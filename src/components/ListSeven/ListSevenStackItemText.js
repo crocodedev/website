76f2b4acdaff090f3477702b62c333f83859b571
fadeeva-codedot <fadeeva.codedot@gmail.com>
2022-06-23 14:30:30 +0300

    23.06. list seven

---
+++ src/components/ListSeven/ListSevenStackItemText.js
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+import Text from "../Text";
+
+const ListSevenStackItemText = styled(Text)`
+  max-width: 686px;
+  margin-bottom: auto;
+`;
+
+export default ListSevenStackItemText;
