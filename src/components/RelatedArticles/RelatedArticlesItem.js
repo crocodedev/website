d4d99e6a956f5b43c2b8d65ecc10957b13fddfe2
fadeeva-codedot <fadeeva.codedot@gmail.com>
2022-06-27 12:38:43 +0300

    27.06. fix slider

--- src/components/RelatedArticles/RelatedArticlesItem.js
+++ src/components/RelatedArticles/RelatedArticlesItem.js
@@ -3,12 +3,15 @@ import styled from "@emotion/styled";
 const RelatedArticlesItem = styled.div`
   display: flex;
   flex-direction: column;
+  scroll-snap-align: start;
 
   ${({ theme }) => theme.breakpoints.down("md")} {
+    flex: 0 0 calc(100% - 20px);
     gap: 10px;
   }
 
   ${({ theme }) => theme.breakpoints.up("md")} {
+    flex: 0 0 calc(50% - 20px);
     gap: 20px;
   }
 `;
