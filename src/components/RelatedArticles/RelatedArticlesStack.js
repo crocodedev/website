d913a4ee326b59dbf023cbc6091b75187edc176b
fadeeva-codedot <fadeeva.codedot@gmail.com>
2022-06-27 11:03:55 +0300

    27.06. added swiper

--- src/components/RelatedArticles/RelatedArticlesStack.js
+++ src/components/RelatedArticles/RelatedArticlesStack.js
@@ -1,16 +1,10 @@
 import styled from "@emotion/styled";
 
 const RelatedArticlesStack = styled.div`
-  display: grid;
+  width: 100%;
 
-  ${({ theme }) => theme.breakpoints.down("lg")} {
-    gap: 30px;
-    grid-template-rows: 1fr 1fr;
-  }
-
-  ${({ theme }) => theme.breakpoints.up("lg")} {
-    gap: 40px;
-    grid-template-columns: 1fr 1fr;
+  .related-articles-swiper {
+    width: 100%;
   }
 `;
 
