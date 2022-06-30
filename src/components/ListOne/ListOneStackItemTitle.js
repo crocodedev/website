e6eacc8c0745573723ad387c6995a3d81f715b91
fadeeva-codedot <fadeeva.codedot@gmail.com>
2022-06-30 14:18:13 +0300

    30.06 List one adaptive

--- src/components/ListOne/ListOneStackItemTitle.js
+++ src/components/ListOne/ListOneStackItemTitle.js
@@ -4,6 +4,10 @@ const ListOneStackItemTitle = styled.div`
   display: flex;
   align-items: center;
   gap: 10px;
+
+  ${({ theme }) => theme.breakpoints.down("md")} {
+    flex-direction: column;
+  }
 `;
 
 export default ListOneStackItemTitle;
