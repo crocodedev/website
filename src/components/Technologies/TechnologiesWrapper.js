4cf1180ea5c2b9b5872d513e7bc3f972ddb35b2f
Daniil Pereverzev <73872354+pereverzev-codedot@users.noreply.github.com>
2022-07-08 09:57:00 +0300

    Update TechnologiesWrapper.js

--- src/components/Technologies/TechnologiesWrapper.js
+++ src/components/Technologies/TechnologiesWrapper.js
@@ -4,10 +4,12 @@ const TechnologiesWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
+  
   ${({ theme }) => theme.breakpoints.down("md")} {
-    gap: 0px;
+    gap: 0;
     width: 100%;
   }
+  
   ${({ theme }) => theme.breakpoints.up("md")} {
     gap: 20px;
   }
