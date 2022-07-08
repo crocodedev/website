7ad7ecb4ef311fe4e529986583154b5fc6d8a57c
Daniil Pereverzev <73872354+pereverzev-codedot@users.noreply.github.com>
2022-07-08 09:36:11 +0300

    Update ProjectsNavRowWrapper.js

--- src/components/Projects/ProjectsNavRowWrapper.js
+++ src/components/Projects/ProjectsNavRowWrapper.js
@@ -2,16 +2,19 @@ import styled from "@emotion/styled";
 
 const ProjectsNavRowWrapper = styled.div`
   display: flex;
+  
   ${({ theme }) => theme.breakpoints.down("md")} {
     max-width: 100%;
     overflow-x: auto;
     padding: 0;
     gap: 10px;
+    
     ::-webkit-scrollbar {
       width: 0;
       height: 0;
     }
   }
+  
   ${({ theme }) => theme.breakpoints.up("md")} {
     gap: 20px;
   }
