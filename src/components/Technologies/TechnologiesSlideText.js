c64e4217b8341f8feec3e3a30018b1daa4fcedd0
Daniil Pereverzev <73872354+pereverzev-codedot@users.noreply.github.com>
2022-07-08 09:55:18 +0300

    Update TechnologiesSlideText.js

--- src/components/Technologies/TechnologiesSlideText.js
+++ src/components/Technologies/TechnologiesSlideText.js
@@ -3,9 +3,11 @@ import Text from "../Text";
 
 const TechnologiesSlideText = styled(Text)`
   max-width: 610px;
+  
   ${({ theme }) => theme.breakpoints.down("xl")} {
     line-height: ${({ theme }) => theme.typography.lineHeight.md};
   }
+  
   ${({ theme }) => theme.breakpoints.up("xl")} {
     line-height: ${({ theme }) => theme.typography.lineHeight.lg};
   }
