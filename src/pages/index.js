99a9b1dab9caf8aafe658471e5056753dd5623b8
dmitry-crocode <dmitryn.crocode@gmail.com>
2022-07-25 10:24:20 +0300

    add other sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,32 +6,32 @@ import theme from "@/theme";
 import reboot from "@/styles/reboot";
 import fontMontserrat from "@/styles/fontMontserrat";
 
-// import SliderSteps from "@/sections/SliderSteps";
-// import BlogList from "@/sections/BlogList";
-// import TextOne from "@/sections/TextOne";
-// import ListOne from "@/sections/ListOne";
-// import ListTwo from "@/sections/ListTwo";
-// import ListThree from "@/sections/ListThree";
-// import ListFour from "@/sections/ListFour";
-// import ListFive from "@/sections/ListFive";
-// import ListSix from "@/sections/ListSix";
-// import ListSeven from "@/sections/ListSeven";
-// import Projects from "@/sections/Projects";
-// import OurTeam from "@/sections/OurTeam";
-// import FAQ from "@/sections/FAQ";
-// import OurClients from "@/sections/OurClients";
-// import Contacts from "@/sections/Contacts";
-// import RelatedArticles from "@/sections/RelatedArticles";
-// import Reviews from "@/sections/Reviews";
+import SliderSteps from "@/sections/SliderSteps";
+import BlogList from "@/sections/BlogList";
+import TextOne from "@/sections/TextOne";
+import ListOne from "@/sections/ListOne";
+import ListTwo from "@/sections/ListTwo";
+import ListThree from "@/sections/ListThree";
+import ListFour from "@/sections/ListFour";
+import ListFive from "@/sections/ListFive";
+import ListSix from "@/sections/ListSix";
+import ListSeven from "@/sections/ListSeven";
+import Projects from "@/sections/Projects";
+import OurTeam from "@/sections/OurTeam";
+import FAQ from "@/sections/FAQ";
+import OurClients from "@/sections/OurClients";
+import Contacts from "@/sections/Contacts";
+import RelatedArticles from "@/sections/RelatedArticles";
+import Reviews from "@/sections/Reviews";
 import Footer from "@/sections/Footer";
-// import Benefits from "@/sections/Benefits";
-// import CtaText from "@/sections/CtaText";
-// import CtaForm from "@/sections/CtaForm";
-// import CtaImage from "@/sections/CtaImage";
-// import Technologies from "@/sections/Technologies";
-// import Development from "@/sections/Development";
-// import TextTwo from "@/sections/TextTwo";
-// import TextThree from "@/sections/TextThree";
+import Benefits from "@/sections/Benefits";
+import CtaText from "@/sections/CtaText";
+import CtaForm from "@/sections/CtaForm";
+import CtaImage from "@/sections/CtaImage";
+import Technologies from "@/sections/Technologies";
+import Development from "@/sections/Development";
+import TextTwo from "@/sections/TextTwo";
+import TextThree from "@/sections/TextThree";
 import ContactUs from "@/sections/ContactUs";
 
 import "destyle.css/destyle.css";
@@ -40,7 +40,7 @@ const HomePage = () => (
   <ThemeProvider theme={theme}>
     <Helmet defer={false} title="HomePage" />
     <Global styles={[reboot, fontMontserrat]} />
-    {/* <Projects />
+    <Projects />
     <SliderSteps />
     <Technologies />
     <Development />
@@ -64,7 +64,7 @@ const HomePage = () => (
     <FAQ />
     <OurClients />
     <Reviews />
-    <Benefits /> */}
+    <Benefits />
     <ContactUs />
     <Footer />
   </ThemeProvider>
