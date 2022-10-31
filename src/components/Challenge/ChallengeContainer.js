abb9fb1671f0fa3bfda33545e5d448cb0f537db2
Eugenia Pereverzeva <fadeeva.codedot@gmail.com>
2022-10-31 12:53:51 +0300

    add white shadow at the end of the section

--- src/components/Challenge/ChallengeContainer.js
+++ src/components/Challenge/ChallengeContainer.js
@@ -2,6 +2,17 @@ import styled from "@emotion/styled";
 import Container from "../Container";
 
 const ChallengeContainer = styled(Container)`
+  position: relative;
+
+  &:after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 35%;
+    background: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, #ffffff 100%);
+    bottom: 0;
+  }
+
   ${({ theme }) => theme.breakpoints.down("xl")} {
     gap: 20px;
   }
