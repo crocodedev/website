dc81110684e6fc152c090e3bb7de27edc4f27f7c
Vadim <vadimm.crocode@gmail.com>
2023-01-10 08:47:01 +0100

    fix

--- src/components/RelatedArticles/RelatedArticlesImageWrapper.js
+++ src/components/RelatedArticles/RelatedArticlesImageWrapper.js
@@ -4,6 +4,22 @@ const RelatedArticlesImageWrapper = styled.div`
   overflow: hidden;
   border-radius: ${({ theme }) => theme.borderRadius.small};
 
+  ${({ theme }) => theme.breakpoints.down("md")} {
+    height: 220px;
+  }
+
+  ${({ theme }) => theme.breakpoints.between("md", "xl")} {
+    height: 240px;
+  }
+
+  > div {
+    height: 100%;
+  }
+
+  ${({ theme }) => theme.breakpoints.up("xl")} {
+    height: 360px;
+  }
+
   & img {
     width: 100%;
     height: 100%;
