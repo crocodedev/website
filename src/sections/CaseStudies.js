1946543c6660ef607c92aad16c345153905bec14
Vadim <vadimm.crocode@gmail.com>
2023-01-12 13:21:49 +0100

    fix

--- src/sections/CaseStudies.js
+++ src/sections/CaseStudies.js
@@ -3,21 +3,21 @@ import Container from "@/components/Container";
 import SectionHeading from "@/components/SectionHeading";
 import SectionWrapper from "@/components/SectionWrapper";
 import Button from "@/components/Button";
-import ProjectsStack from "@/components/Projects/ProjectsStack";
-import Card from "@/components/Card";
-import { Link } from "gatsby";
+// import ProjectsStack from "@/components/Projects/ProjectsStack";
+// import Card from "@/components/Card";
+// import { Link } from "gatsby";
 
 const CaseStudies = ({ title, subtitle, items, link, baseUrl }) => (
   <SectionWrapper>
     <Container>
       <SectionHeading title={title} text={subtitle} />
-      <ProjectsStack>
+      {/* <ProjectsStack>
         {items?.map(({ _key, slug, ...info }) => (
           <Link key={_key} to={slug.current}>
             <Card {...info} />
           </Link>
         ))}
-      </ProjectsStack>
+      </ProjectsStack> */}
       {link && (
         <Button aria-label="Show all case studies" baseUrl={baseUrl} link={link} variant="outlined">
           {link.title}
