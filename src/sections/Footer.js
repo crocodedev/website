7af1094650158929f58937ea0bc0c647663a1727
Vadim <vadimm.crocode@gmail.com>
2023-01-20 12:36:27 +0100

    fix

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -34,7 +34,7 @@ const Footer = ({ copyrightText, logoImage, socialIcons, column, bottomLinks, ba
           <FooterTopStack>
             {column?.map(({ title, _key, items, link: topLink }) => (
               <FooterTopColumn key={_key}>
-                <FooterLink active="true" baseUrl={baseUrl} {...topLink}>
+                <FooterLink isheading baseUrl={baseUrl} {...topLink}>
                   {title}
                 </FooterLink>
                 {items?.map(({ _key: id, link }) => (
@@ -51,7 +51,7 @@ const Footer = ({ copyrightText, logoImage, socialIcons, column, bottomLinks, ba
           <Text color={theme.palette.tertiary.contrastText}>{copyrightText}</Text>
           <div>
             {bottomLinks?.map((link) => (
-              <FooterLink {...link} baseUrl={baseUrl} key={link._key} active="true">
+              <FooterLink {...link} baseUrl={baseUrl} key={link._key} isheading>
                 {link.title}
               </FooterLink>
             ))}
