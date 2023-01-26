025af981ce01de8812743aa14879bbd3cc40f92a
Vadim <vadimm.crocode@gmail.com>
2023-01-26 14:19:57 +0100

    fix

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -6,11 +6,18 @@ export default {
         title: "My Netlify deploys",
         sites: [
           {
-            title: "Website",
-            apiId: "a996a672-d817-4ec0-bfce-8bce064542a5",
-            buildHookId: "6388b9d99cfebc02527a19e8",
+            title: "Crocode production",
+            apiId: "15b38e50-7eb6-4452-87d2-9752abb5b9c8",
+            buildHookId: "63d27d651ae7af0221ff30fa",
             name: "Crocode",
-            url: "https://crocode-test.netlify.app",
+            url: "https://crocode.io",
+          },
+          {
+            title: "Crocode develop",
+            apiId: "27ff182b-a80b-4082-bad2-8739b798050f",
+            buildHookId: "63d27db731e1bd06a5782e16",
+            name: "Crocode",
+            url: "https://crocode-develop.netlify.app",
           },
         ],
       },
