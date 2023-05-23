80efe8d97b5463b9d16ddfaad83dc39b0689e400
Daniil Yurkevich <yurkevich.crocode@gmail.com>
2023-05-23 16:18:10 +0300

    update deskSctructure, create redirect

--- studio/schemas/documents/redirect.js
+++ studio/schemas/documents/redirect.js
@@ -1,8 +1,46 @@
+import { MasterDetailIcon } from "@sanity/icons";
+
 export default {
+  preview: {
+    select: {
+      title: "fromPath",
+    },
+  },
+
   title: "Redirect",
   name: "redirect",
   type: "document",
+  icon: MasterDetailIcon,
+  i18n: {
+    fieldNames: {
+      lang: "i18n_lang",
+      baseReference: "i18n_base",
+      references: "i18n_refs",
+    },
+  },
   fields: [
+    {
+      name: "i18n_lang",
+      type: "string",
+      hidden: true,
+    },
+    {
+      name: "i18n_base",
+      type: "reference",
+      to: [{ type: "redirect" }],
+      hidden: true,
+    },
+    {
+      name: "i18n_refs",
+      type: "array",
+      hidden: true,
+      of: [
+        {
+          type: "reference",
+          to: [{ type: "redirect" }],
+        },
+      ],
+    },
     {
       title: "From Path",
       name: "fromPath",
