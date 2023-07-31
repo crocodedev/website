0c09683e82037cae7c6c55e3557e3c03e254b348
Daniil <daniilp.crocode@gmail.com>
2023-07-31 10:07:56 +0300

    added link in techologyCard

--- src/components/TechnologyCard.js
+++ src/components/TechnologyCard.js
@@ -1,11 +1,17 @@
+import { Link } from "gatsby";
+
 import PropTypes from "prop-types";
 import Image from "./Image";
 import TechnologyCardItem from "./TechnologyCard/TechnologyCardItem";
 import Text from "./Text";
 
-const TechnologyCard = ({ image, title, variant }) => {
+const TechnologyCard = ({ image, title, variant, link }) => {
   return (
-    <TechnologyCardItem variant={variant}>
+    <TechnologyCardItem
+      as={variant === "transparent" ? Link : "div"}
+      to={link?.linkInternal?.reference?.slug?.current}
+      variant={variant}
+    >
       <Image {...image} />
       <Text lineHeight="md" fontSize="text" fontWeight="regular" mobileMultiplier={0.75}>
         {title}
@@ -18,10 +24,12 @@ TechnologyCard.propTypes = {
   image: PropTypes.object.isRequired,
   title: PropTypes.string.isRequired,
   variant: PropTypes.string,
+  link: PropTypes.string,
 };
 
 TechnologyCard.defaultProps = {
   variant: "",
+  link: "",
 };
 
 export default TechnologyCard;
