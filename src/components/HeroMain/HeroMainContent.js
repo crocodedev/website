304f3e76166a8ca4f65e458b1355ebb6e7167bce
Eugenia Fadeeva <fadeeva.codedot@gmail.com>
2024-05-30 02:23:23 +0200

    fixes

--- src/components/HeroMain/HeroMainContent.js
+++ src/components/HeroMain/HeroMainContent.js
@@ -5,36 +5,11 @@ const HeroMainContent = styled.div`
   flex-direction: column;
   align-items: center;
 
-  ${({ theme }) => theme.breakpoints.down("sm")} {
-    height: 230px;
+  ${({ theme }) => theme.breakpoints.down("lg")} {
     gap: 20px;
   }
 
-  ${({ theme }) => theme.breakpoints.between("sm", "md")} {
-    height: 240px;
-    gap: 20px;
-  }
-
-  ${({ theme }) => theme.breakpoints.between("md", "lg")} {
-    height: 340px;
-    gap: 20px;
-  }
-
-  ${({ theme }) => theme.breakpoints.between("lg", "xl")} {
-    height: 500px;
-    padding-top: 100px;
-    gap: 40px;
-  }
-
-  ${({ theme }) => theme.breakpoints.between("xl", "2xl")} {
-    height: 670px;
-    padding-top: 140px;
-    gap: 40px;
-  }
-
-  ${({ theme }) => theme.breakpoints.up("2xl")} {
-    height: 820px;
-    padding-top: 172px;
+  ${({ theme }) => theme.breakpoints.up("lg")} {
     gap: 40px;
   }
 `;
--- src/components/HeroMain/HeroMainWrapper.js
+++ src/components/HeroMain/HeroMainWrapper.js
@@ -3,6 +3,8 @@ import styled from "@emotion/styled";
 const HeroMainWrapper = styled.section`
   position: relative;
   z-index: 0;
+  min-height: calc(100vh - 100px);
+  padding-bottom: 50px;
 `;
 
 export default HeroMainWrapper;
